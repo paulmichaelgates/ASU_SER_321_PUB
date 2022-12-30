c5d43a0957cc7ea3984180b87343106d008c7d4e
paul <115600972+pmgates42@users.noreply.github.com>
2022-12-29 23:55:15 -0700

    UDP basic sender and receiver examples completed, not the best implementation but I will clean it up later

--- UserDatagramProtocol/Receiver/Receiver.java
+++ UserDatagramProtocol/Receiver/Receiver.java
@@ -1,3 +1,4 @@
+import java.io.DataInputStream;
 import java.lang.IllegalArgumentException;
 
 import java.net.DatagramSocket;
@@ -13,31 +14,33 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 public class Receiver {
-    
-    static final int STD_DATA_SIZE  = 256; /* 256 byte data limit */
-    static final int PORT_NUMBER    = 8000; /* port used for socket */
 
-    /* define socket which is the software interface to the networking 
-    * protocols responsible for the rx and tx of data packets
-    */
+    static final int STD_DATA_SIZE = 256; /* 256 byte data limit */
+    static final int PORT_NUMBER = 8000; /* port used for socket */
+
+    /*
+     * define socket which is the software interface to the networking
+     * protocols responsible for the rx and tx of data packets
+     */
     static DatagramSocket dataSocket = null;
     static DatagramPacket packet = null;
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        try{
-            dataSocket = new DatagramSocket( PORT_NUMBER );
-        }catch(SocketException e){
+        try {
+            dataSocket = new DatagramSocket(PORT_NUMBER);
+        } catch (SocketException e) {
             e.printStackTrace();
         }
 
-        /* 
+        /*
          * create an array of bytes (buffer pointer) which we can use to store
          * the data we will send/receive
          */
-        byte[] data = new byte[ STD_DATA_SIZE ];
+        byte[] data = new byte[STD_DATA_SIZE];
 
-        /* create a datagram packet which will act as the container for all
+        /*
+         * create a datagram packet which will act as the container for all
          * of our sending and receiving data. i.e., we will use this same object
          * for when we get data from some other application (Sender.java) that we
          * will use to send data to.
@@ -48,39 +51,41 @@ public class Receiver {
     }
 
     /* method to recieve packet */
-    static void rxPacketData(){
-        /* although it may seem like magic, receiving data is a blocking operation
+    static void rxPacketData() {
+        /*
+         * although it may seem like magic, receiving data is a blocking operation
          * which means that the program will hang until something allows it to continue
          * 
          * this is kind of like a busy loop ( e.g., while(1); ) or attempting to aquire
          * a lock for a semaphore or mutex
          */
-        try{
-            
-            if(dataSocket != null){
+        try {
+
+            if (dataSocket != null) {
                 dataSocket.receive(packet);
-            }else{
-                throw new IllegalArgumentException("dataSocket was not initialized correctly");    
+            } else {
+                throw new IllegalArgumentException("dataSocket was not initialized correctly");
             }
-        
-        }catch(IOException e){
+
+        } catch (IOException e) {
             e.printStackTrace();
 
-        }catch(IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
             e.printStackTrace();
 
         }
 
         System.out.println("packet was recieved on port " + PORT_NUMBER);
-    
+
         /* open up the packet contents */
         parsePacketData();
     }
 
     /* parse out the data that was received */
-    static void parsePacketData(){
+    static void parsePacketData() {
 
-        /* get the address and put in a (sometimes) nicer string
+        /*
+         * get the address and put in a (sometimes) nicer string
          * format
          */
         InetAddress remote_address = packet.getAddress();
@@ -89,38 +94,32 @@ public class Receiver {
         System.out.println("Sender address: " + sender_address);
         System.out.println("Sent from port: " + packet.getPort());
 
-        /* wrap the data packet usin a ByteArrayInputStream which is
+        /*
+         * wrap the data packet usin a ByteArrayInputStream which is
          * inherits from DataInputStream (inheriting from lower level
          * InputStream -- see DataStreams directory for this repo)
          * and is used to create an easy interface to reading byte streamed
          * data
-        */
-        ByteArrayInputStream byteIStream = new ByteArrayInputStream(packet.getData()); 
+         */
+        ByteArrayInputStream byteIStream = new ByteArrayInputStream(packet.getData());
 
-        /* display the data
+        /*
+         * display the data
          * note that we only want to display the length of the packet
          * this value is effectively set by the sender, that is, it is
          * within the packet and then the java.net.DataSocket method
          * to recieve packets will set the length after parsing the packet
          */
-        for(int i = 0; i < packet.getLength(); i++){
-            
+        for (int i = 0; i < packet.getLength(); i++) {
             int data = byteIStream.read();
-            
-            /*  even though we have a specified length, a negative -1
-                is the InputStreams way of saying "no more data left"
-                and thus saving from reading invalid data
-             */
-            if(data == -1)
-            {
+
+            if (data == -1) {
                 break;
             }
 
-            System.out.print(data);
+            System.out.print((char) data);
         }
-        
-        dataSocket.close();        
+
+        dataSocket.close();
     }
 }
-
-
--- UserDatagramProtocol/sender/Sender.java
+++ UserDatagramProtocol/sender/Sender.java
@@ -1,5 +1,125 @@
-package UserDatagramProtocol.sender;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
 public class Sender {
-    
+
+    /*
+     * constants -- may need to change depending on if any of these are currently
+     * in service
+     */
+    static final int RX_PORT_NUMBER = 2000; /* port that our sender socket lives on */
+    static final int TX_PORT_NUMBER = 8000; /* port that we will be sending to */
+
+    /* module variables */
+    static DatagramSocket socket = null;
+
+    public static void main(String[] args) {
+
+        /*
+         * get the data args (this is the data
+         * that will be sent)
+         */
+        if (args.length < 2) {
+            System.out.println("Sending data was not specified");
+            System.exit(1);
+        }
+
+        /*
+         * takes the first argument given during execution
+         * and put into a byte array that will eventually
+         * be stored in packet and send to receiver
+         */
+        byte[] data = args[0].getBytes();
+
+        /* host should be the second argument */
+        String host_name = args[1];
+
+        /*
+         * initialize the datagram packet and
+         * datagram socket which will be used
+         * for sending the data out to specified
+         * InetAddress
+         */
+        DatagramPacket packet = new DatagramPacket(data, data.length);
+
+        try {
+            socket = new DatagramSocket(RX_PORT_NUMBER);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+
+        /* send the packet */
+        int err = setUpDatagramPacket(packet, data, host_name);
+        if (err != 0) {
+            System.out.println("Data packet would not be sent. " +
+                    "While setting up packet returned error of" + err);
+
+            System.exit(1);
+        }
+
+        /* send the packet */
+        sendPacket(packet);
+    }
+
+    /*
+     * prepares the data to be sent
+     * 
+     * loads info such as:
+     * - proper address and port info
+     * - actual data
+     */
+    static int setUpDatagramPacket(DatagramPacket packet, byte[] buffer, String host_name) {
+
+        /* set up data and size */
+        packet.setLength(buffer.length);
+        packet.setData(buffer);
+
+        /* set up networking details */
+        InetAddress remote_address = getHostName(host_name);
+
+        if (host_name == null) {
+            return 1;
+        }
+
+        packet.setAddress(remote_address);
+        packet.setPort(TX_PORT_NUMBER);
+
+        /*
+         * indicate that there was no error setting up
+         * the datagram packet
+         */
+        return 0;
+
+    }
+
+    /* helper to get the host name */
+    static InetAddress getHostName(String host_name) {
+
+        /* get the local host */
+        InetAddress remote_address = null;
+        try {
+            remote_address = InetAddress.getByName(host_name);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+        return remote_address;
+    }
+
+    /* send packet helper */
+    static void sendPacket(DatagramPacket packet) {
+
+        /* send the packet and catch any IO errors */
+        try {
+            socket.send(packet);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
